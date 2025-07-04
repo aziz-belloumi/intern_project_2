96d3b8a65be3d24f992ab0d08969645d1dd08154
aziz-belloumi <belloumiaziz1@gmail.com>
2025-07-04 16:08:20 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-//import { TitleComponent } from './components/title/title.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
